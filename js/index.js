264725a69fa8444e68d7fec4061bbd80a35dba7a
аппле <apple@MacBook-Air-apple.local>
2024-10-10 07:15:06 +0500

    Javascript

---
+++ js/index.js
@@ -0,0 +1,80 @@
+// Massiv Masalalar
+
+// 1-masala
+// let vegetables = ["Cucumber", "tomato", "potato", "pumpkin"];
+// console.log(vegetables[0]);
+// console.log(vegetables[3]);
+
+// 2-masala
+
+// let fruits = ["banana", "apple" , "apricot" , "peach" , "changer"];
+// fruits[4] = "Salom";
+// console.log(fruits);
+
+// 3-masala
+
+// let fruits = []
+// fruits[0] = "Banana";
+// fruits[1] = "Apple" ;
+// fruits[2] = "Lemon" ;
+// console.log(fruits);
+
+// 4-masala
+
+// let phones = ["Iphone" , "Samsung" , "Redmi" , "Poco" , "Infinix"];
+// console.log(phones.length);
+
+// 5-masala
+
+// let computers =["Mackbook", "Acer", "Samsung" ] ;
+// computers.push("Linux");
+// console.log(computers);
+
+// 6-masala
+
+// let people = ["introvert" , "extrovert" , "ambiavent"];
+// people.pop();
+// console.log(people);
+
+// 7-masala
+
+// let emotions = ["sad" , "happy" , "funny", ];
+// emotions.unshift("loser");
+// console.log(emotions);
+
+// 8-masala
+
+// let currencies = ["dollar", "euro", "soums", "won", "Singapore dollar"];
+// currencies.shift()
+// console.log(currencies);
+
+// 9-masala
+
+// let numbers = [2,4,6,7,8,9];
+// for (i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// 10-masala
+
+let massive = [12, 34, "salom" , "xayr", true, false];
+let value = prompt("Qiymatni kirting");
+
+if (value === massive[0,1,2,3,4,5]) {
+    console.log("Bu qiymat mavjud"); 
+} else {
+    console.log("Bu qiymat mavjud emas");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
